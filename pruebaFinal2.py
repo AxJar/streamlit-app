3c7ae695dbccc92fdcc344528e2c32fdadde352a
Axel Jarquin <a01636324@itesm.mx>
2023-06-15 18:04:39 -0600

    Update pruebaFinal2.py

--- pruebaFinal2.py
+++ pruebaFinal2.py
@@ -12,6 +12,9 @@ from sklearn.preprocessing import PolynomialFeatures
 from sklearn.linear_model import LinearRegression
 import numpy as np
 import pickle
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix
 
 @st.cache_data
 def get_data():
